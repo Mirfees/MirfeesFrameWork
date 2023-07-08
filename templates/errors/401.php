a65f11ca815b6a0a9dabecd79ec13fbee5539131
Miron <mirfees@gmail.com>
2023-07-08 18:07:08 +0600

    init commit

--- templates/errors/401.php
+++ templates/errors/401.php
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 <?php include __DIR__ . '/../header.php'; ?>
     <h1>Вы не авторизованы</h1>
     Для доступа к этой странице нужно <a href="/users/login">войти на сайт</a>
+=======
+<?php include __DIR__ . '/../header.php'; ?>
+    <h1>Вы не авторизованы</h1>
+    Для доступа к этой странице нужно <a href="/users/login">войти на сайт</a>
+>>>>>>> 1d6c06d (init commit)
 <?php include __DIR__ . '/../footer.php'; ?>
