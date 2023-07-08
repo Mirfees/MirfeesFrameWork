6639ca33e5dba852f6e0123e932cd9c63b7f0ef1
Miron <mirfees@gmail.com>
2023-07-08 21:07:31 +0600

    Added display of buttons for comments

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -2,11 +2,21 @@
     <h1><?= $article->getName() ?></h1>
     <p><?= $article->getText() ?></p>
     <p>Автор: <?php echo $article->getAuthor()->getNickname();  ?></p>
+
+
     <?php if (!empty($user)): ?>
+    <form action="/articles/<?= $article->getId() ?>/comments" method="post">
+        <label for="commentText">Оставить комментарий</label><br>
+        <textarea name="commentText" id="text" rows="10" cols="80"><?= $_POST['commentText'] ?? '' ?></textarea><br>
+        <br>
+        <input type="submit" value="Отправить комментарий">
+    </form>
         <?php if ($user->getRole() === 'admin'): ?>
         <p>
             <a href="<?= $article->getId() . '/edit'?>">Редактировать статью</a>
         </p>
         <?php endif; ?>
+    <?php elseif (empty($user)): ?>
+    <a href="/users/login">Войдите в аккаунт, чтобы комментировать</a>
     <?php endif; ?>
 <?php include __DIR__ . '/../footer.php'; ?>
