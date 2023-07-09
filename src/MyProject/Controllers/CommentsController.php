f5411065844630d88e24a5797a96aa954aa4cb5e
Miron <mirfees@gmail.com>
2023-07-09 20:26:10 +0600

    Added docs and methods edit() and delete in CommentsController.php

--- src/MyProject/Controllers/CommentsController.php
+++ src/MyProject/Controllers/CommentsController.php
@@ -13,25 +13,77 @@ use MyProject\Models\Users\User;
 
 class CommentsController extends AbstractController
 {
-    public function addComment($articleId)
+    /**
+     * @param $articleId
+     * @return void
+     * @throws UnauthorizedException
+     */
+    public function add ($articleId)
     {
-
         if ($this->user === null) {
             throw new UnauthorizedException('Необходимо зайти в аккаунт');
         }
 
         if (!empty($_POST)) {
             try {
-                var_dump($_POST['commentText']);
-                var_dump($articleId);
-                $commentText = $_POST['commentText'];
-                Comment::addComment($commentText, $this->user, $articleId);
+                $commentText = $_POST;
+                Comment::add($commentText, $this->user, $articleId);
             } catch (InvalidArgumentException $e) {
-                $this->view->renderHtml('/errors/500.php', ['error' => $e->getMessage()]);
+                $this->view->renderHtml('errors/500.php', ['error' => $e->getMessage()]);
                 return;
             }
+            header('Location: /articles/' . $articleId, true, 302);
+            exit();
+        }
+    }
+
+    /**
+     * @param $commentId
+     * @return void
+     * @throws ForbiddenException
+     * @throws UnauthorizedException
+     */
+    public function edit ($commentId): void
+    {
+        $comment = Comment::getById($commentId);
+
+        if ($comment === null) {
+            throw new ForbiddenException('Не найдено комментария');
         }
 
+        if ($this->user === null) {
+            throw new UnauthorizedException('Необходимо зайти в аккаунт');
+        }
+
+        if ($this->user->getRole() !== 'admin' or $comment->getAuthorId() != $this->user->getId()) {
+            throw new ForbiddenException('Недостаточно прав!');
+        }
+
+        if(!empty($_POST)) {
+            try {
+                $articleId = $comment->getArticleId();
+                $comment->edit($_POST);
+            } catch (InvalidArgumentException $e) {
+                $this->view->renderHtml('comments/edit.php', ['error' => $e->getMessage(), 'comment' => $comment]);
+                return;
+            }
+
+            header('Location: /articles/' . $comment->getArticleId() . '#' . $comment->getId(), true, 302);
+            exit();
+        }
+
+        $this->view->renderHtml('comments/edit.php', ['comment' => $comment]);
+    }
+
+    public function delete ($commentId): void
+    {
+        if ($this->user->getRole() !== 'admin') {
+            throw new ForbiddenException('Недостаточно прав!');
+        }
+
+        $comment = Comment::getById($commentId);
+
+        $comment->delete();
 
     }
 }
