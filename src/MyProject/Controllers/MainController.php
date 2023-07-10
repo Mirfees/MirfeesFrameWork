5e5928ca27ca88485b9fb7e88e29ca38529668ce
Miron <mirfees@gmail.com>
2023-07-11 00:03:07 +0600

    Deleted unused methods, added mainAdminer() for MainController.php

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -2,6 +2,8 @@
 
 namespace MyProject\Controllers;
 
+use MyProject\Exceptions\ForbiddenException;
+use MyProject\Exceptions\UnauthorizedException;
 use MyProject\Models\Articles\Article;
 use MyProject\View\View;
 use MyProject\Models\Users\UsersAuthService;
@@ -15,18 +17,16 @@ class MainController extends AbstractController
         $this->view->renderHtml('main/main.php', ['articles' => $articles, 'title' => $title, 'user' => UsersAuthService::getUserByToken()]);
     }
 
-    public function sayHello(string $name)
+    public function mainAdminer()
     {
-        $title = 'Страница приветствия';
-        $this->view->renderHtml('main/hello.php',   [   'name'    =>  $name,
-                                                                    'title'   =>  $title,
-                                                                ]);
-    }
+        if ($this->user === null) {
+            throw new UnauthorizedException('Необходимо войти в аккаунт');
+        }
 
-    public function sayBye(string $name)
-    {
-        $title = 'Страница прощания';
-        $this->view->renderHtml('main/bye.php', [   'name'  => $name,
-                                                                'title' =>  $title,]);
+        if ($this->user->getRole() !== 'admin') {
+            throw new ForbiddenException('Недостаточно прав!');
+        }
+
+        $this->view->renderHtml('/../templates/adminer/main/main.php', [], 200);
     }
 }
