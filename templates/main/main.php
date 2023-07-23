250c5048c54e3c37704156d5749ffdb98a3e691f
Miron <mirfees@gmail.com>
2023-07-23 23:02:29 +0600

    Limited output of text at the main page

--- templates/main/main.php
+++ templates/main/main.php
@@ -6,7 +6,7 @@
 include __DIR__ . '/../header.php'; ?>
 <?php foreach ($articles as $article): ?>
     <h2><a href="/articles/<?= $article->getId() ?>"><?= $article->getName() ?></a></h2>
-    <p><?= $article->getText() ?></p>
+    <p><?= substr($article->getText(), 0, 200) ?></p>
     <hr>
 <?php endforeach; ?>
 <?php include __DIR__ . '/../footer.php'; ?>
