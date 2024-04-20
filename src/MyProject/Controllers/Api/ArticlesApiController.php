48a38cd344803cab53498a319bc291cf09d5e778
Miron Fissenko <mirfees@gmail.com>
2024-04-20 16:53:48 +0500

    added new method add

--- src/MyProject/Controllers/Api/ArticlesApiController.php
+++ src/MyProject/Controllers/Api/ArticlesApiController.php
@@ -4,7 +4,10 @@ namespace MyProject\Controllers\Api;
 
 use MyProject\Controllers\AbstractController;
 use MyProject\Exceptions\NotFoundException;
+use MyProject\Exceptions\UnauthorizedException;
 use MyProject\Models\Articles\Article;
+use MyProject\Models\Users\User;
+use MyProject\Models\Users\UsersAuthService;
 
 class ArticlesApiController extends AbstractController
 {
@@ -27,13 +30,25 @@ class ArticlesApiController extends AbstractController
     }
 
     public function add() {
-        if($_SERVER["REQUEST_METHOD"]) {
-            $input = json_decode(
-                file_get_contents('php://input'),
-                true
-            );
+        if($_SERVER["REQUEST_METHOD"] === "POST") {
+            $input = $this->getInputData();
+            $articleFromRequest = $input['articles'][0];
+            $currentUser = UsersAuthService::getUserByToken();
 
-            var_dump($input);
+            $authorId = $articleFromRequest['author_id'];
+            if ($currentUser === null) {
+                throw new UnauthorizedException("Войдите в аккаунт, чтобы добавлять статьи");
+            }
+
+            if ($currentUser->getId() !== (int) $authorId) {
+                throw new UnauthorizedException("Добавляйте статьи от своего аккаунта");
+            }
+
+            $author = User::getById($authorId);
+            $article = Article::createFromArray($articleFromRequest, $author);
+            $article->save();
+
+            header('Location: /api/articles/' . $article->getId(), true, 302);
         }
     }
 }
